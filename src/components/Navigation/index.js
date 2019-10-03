843bb1f4462de9f886f5625a6a7273d2fbc4a603
Austin Trow <austintrow@gmail.com>
2019-10-03 12:06:27 -0400

    sign in/ sign out

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+
 const Navigation = () => (
   <div>
     <ul>
@@ -18,6 +21,9 @@ const Navigation = () => (
       </li>
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+      <li>
+        <SignOutButton />
       </li>
     </ul>
   </div>
