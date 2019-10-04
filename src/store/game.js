37a156d6910aec6dcdb415f98098bf0f1e6db41a
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-04 16:58:49 -0400

    problems appearing

--- src/store/game.js
+++ src/store/game.js
@@ -1,14 +1,32 @@
-import firebase from "../components/Firebase";
+import firebase from "../index";
+
 const GOT_CODE = "GOT_CODE";
+const GOT_PROBLEM = "GOT_PROBLEM";
 
 const initialState = {
-  code: ""
+  code: "",
+  problem: ""
 };
 
 const gotCode = code => ({
   type: GOT_CODE,
   code
 });
+const gotProblem = problem => ({
+  type: GOT_PROBLEM,
+  problem
+});
+
+export const getProblem = id => {
+  return async dispatch => {
+    const data = firebase.problem(id);
+    // console.log(firebase.problem(id));
+
+    data.get().then(doc => {
+      dispatch(gotProblem(doc.data().prompt));
+    });
+  };
+};
 
 export const submitCode = code => {
   return async dispatch => {
@@ -20,6 +38,8 @@ const gameReducer = (state = initialState, action) => {
   switch (action.type) {
     case GOT_CODE:
       return { ...state, code: action.code };
+    case GOT_PROBLEM:
+      return { ...state, problem: action.problem };
     default:
       return state;
   }
