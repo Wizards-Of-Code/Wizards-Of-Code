fb25021eb01d3f057544c80c2f6de3eb2eae19d3
Maksym <mfedorenko16@gmail.com>
2019-10-04 15:57:17 -0400

    some home page changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,12 @@ const HomePage = () => (
   <div>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
+    <div className="profile">
+      <h1>Profile</h1>
+    </div>
+    <div className="battle-btn">
+      <button>To the battle</button>
+    </div>
   </div>
 );
 
