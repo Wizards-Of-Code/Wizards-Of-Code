2531d57a45ef4b9147c74310811524c13190ef44
Austin Trow <austintrow@gmail.com>
2019-10-04 17:46:13 -0400

    not working yet

--- src/store/game.js
+++ src/store/game.js
@@ -1,22 +1,38 @@
 import firebase from "../index";
 
-const GOT_CODE = "GOT_CODE";
+// ACTION TYPE
+const GOT_RESULT = "GOT_RESULT";
 const GOT_PROBLEM = "GOT_PROBLEM";
+const GOT_CODE = "GOT_CODE"
 
 const initialState = {
   code: "",
+  result: "",
   problem: ""
 };
 
-const gotCode = code => ({
-  type: GOT_CODE,
-  code
+// ACTION CREATOR
+const gotResult = result => ({
+  type: GOT_RESULT,
+  result
 });
 const gotProblem = problem => ({
   type: GOT_PROBLEM,
   problem
 });
+const gotCode = code => ({
+  type: GOT_CODE,
+  code
+})
 
+export const getCode = code => {
+  return dispatch => {
+    dispatch(gotCode(code))
+  }
+}
+
+
+// THUNK CREATORS
 export const getProblem = id => {
   return async dispatch => {
     const data = firebase.problem(id);
@@ -29,17 +45,30 @@ export const getProblem = id => {
 };
 
 export const submitCode = code => {
-  return async dispatch => {
-    dispatch(gotCode(code));
-  };
+    const webWorker = new Worker('../webWorker/webWorker.js');
+
+    console.log(code);
+
+    webWorker.postMessage({
+      inputs: {input1: [4], input2: [3]},
+      userFunction: code
+    })
+
+    webWorker.onmessage = function(event) {
+      gotResult(event.data);
+      webWorker.terminate();
+    }
 };
+
 
 const gameReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GOT_CODE:
-      return { ...state, code: action.code };
+    case GOT_RESULT:
+      return { ...state, result: action.result };
     case GOT_PROBLEM:
       return { ...state, problem: action.problem };
+    case GOT_CODE:
+      return { ...state, code: action.code};
     default:
       return state;
   }
--- src/webWorker/webWorker.js
+++ src/webWorker/webWorker.js
@@ -1,29 +1,4 @@
-// // makes a new function based on passed parameters function body
-// const makeFunction = ([...funcParams], funcBody) => {
-//   // eslint-disable-next-line no-new-func
-//   let resultingFunction = new Function (...funcParams, funcBody);
-//   return resultingFunction
-// }
-
-// onmessage = function(event) {
-
-//   // create user function
-//   const userFunction = makeFunction(event.data.funcParams, event.data.funcBody)
-
-//   // run user function with all inputs passed as params
-//   const outputs = [];
-//   event.data.inputs.forEach(input => {
-//     try {
-//       outputs.push(userFunction(...input));
-//     } catch (error) {
-//       // return error message if code fails
-//       postMessage(`program crashed on test input: ${input}`)
-//     }
-//   })
-
-//   postMessage(outputs);
-
-// };
+console.log('in web worker');
 
 
 onmessage = function(event) {
@@ -35,7 +10,7 @@ onmessage = function(event) {
 
   eval('userFunction =' + event.data.userFunction)
 
-  event.data.inputs.forEach(input => {
+  Object.values(event.data.inputs).forEach(input => {
     outputs.push(userFunction(...input))
   })
 
