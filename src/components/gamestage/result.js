c5815b981797124ef0e3b1fff16753b7eb24521f
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-04 15:54:15 -0400

    store and submit code button

--- src/components/gamestage/result.js
+++ src/components/gamestage/result.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { submitCode } from "../../store/game";
 
-export default function Result() {
+const DisconnectedResult = props => {
   return (
     <div className="result">
       <div className="title">Result</div>
+      <button className="buttonFont" onClick={() => props.submitCode()}>
+        SUBMIT CODE
+      </button>
     </div>
   );
-}
+};
+const mapDispatchToProps = dispatch => ({
+  submitCode: code => dispatch(submitCode(code))
+});
+const mapStateToProps = state => {
+  return { state: state };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DisconnectedResult);
