72f2529e43506165272e4076cc4e57ec1baa3090
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-06 12:33:20 -0400

    better characters

--- src/components/gamestage/index.js
+++ src/components/gamestage/index.js
@@ -7,21 +7,36 @@ import CodeArea from "./codeArea";
 import Instructions from "./instructions";
 import Result from "./result";
 
-// adjust image size to 1920x540 pixels 
+// adjust image size to 1920x540 pixels
 
 const GameStage = props => {
-  const innerWidth = window.innerWidth
-  const innerHeight = window.innerHeight
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
   return (
     <div>
-      <Stage
-        width={innerWidth}
-        height={innerHeight/1.75}
-      >
+      <Stage width={innerWidth} height={innerHeight / 1.75}>
         <Container>
-          <Sprite anchor={0.5} x={innerWidth/2} y={innerHeight/3.5} image={background} scale={1} />
-          <Sprite anchor={0.5} x={innerWidth/3} y={innerHeight/3} image={mage} scale={2} />
-          <Sprite anchor={0.5} x={innerWidth/1.5} y={innerHeight/3} image={mage2} scale={0.5} />
+          <Sprite
+            anchor={0.5}
+            x={innerWidth / 2}
+            y={innerHeight / 3.5}
+            image={background}
+            scale={1}
+          />
+          <Sprite
+            anchor={0.5}
+            x={innerWidth / 3}
+            y={innerHeight / 3}
+            image={mage}
+            scale={0.5}
+          />
+          <Sprite
+            anchor={0.5}
+            x={innerWidth / 1.5}
+            y={innerHeight / 3}
+            image={mage2}
+            scale={0.5}
+          />
         </Container>
       </Stage>
       <div className="taskbox">
