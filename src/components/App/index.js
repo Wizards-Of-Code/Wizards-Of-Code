ebb425910dff2b60a14de88dce3fbd04611351d0
Austin Trow <austintrow@gmail.com>
2019-10-08 13:10:38 -0400

    get user info from cloudstore on refresh

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -106,7 +106,20 @@ class App extends React.Component {
     };
   };
 
+  componentDidMount () {
+    this.props.firebase.auth.onAuthStateChanged(authUser => {
+      console.log(authUser);
+      if (authUser) {
+        this.login(authUser.uid);
+      }
+    });
+  }
+
+
+
+
   render() {
+
     return (
       <Router>
         <div className="container">
