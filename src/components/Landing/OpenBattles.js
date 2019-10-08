34cab3439798b5c3a1a88622d84590d979c674af
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-08 15:32:53 -0400

    experience/email

--- src/components/Landing/OpenBattles.js
+++ src/components/Landing/OpenBattles.js
@@ -4,17 +4,20 @@ const OpenBattles = props => {
   const battles = props.openBattles;
   console.log(battles);
   return (
-
     <ol className="join-btl">
       {battles.map(battle => (
         <div className="single-game" key={battle.id}>
           <div>
             Opponent: {battle.user1} -
-            <button className="openGame" onClick={() => props.joinOpenBattle(battle.id)}>
-              Joinable Game
-            </button>
+            <Link to={"/"}>
+              <button
+                className="openGame"
+                onClick={() => props.joinOpenBattle(battle.id)}
+              >
+                Joinable Game
+              </button>
+            </Link>
           </div>
-
         </div>
       ))}
     </ol>
