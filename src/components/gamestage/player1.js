fe685aa224c4f031c441d590c663f5c40dbbc10c
Maksym <mfedorenko16@gmail.com>
2019-10-10 14:58:40 -0400

    winner loser pages

--- src/components/gamestage/player1.js
+++ src/components/gamestage/player1.js
@@ -11,7 +11,7 @@ class Player1 extends Component {
     return (
       <div>
         <h1 style={textTransform}>Player1</h1>
-        <div className='helth-bar1'>
+        <div style={textTransform} className="helth-bar1">
           <div className="innerbar-lost1">
             <div className="innerbar-activ1">
               <p className="hp-points1">90%</p>
