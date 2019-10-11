2b86a2ded5af64b92d5b3a7bd2b7f938daa9c1a5
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-10 22:34:43 -0400

    homepage

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -16,40 +16,40 @@ class LandingPage extends React.Component {
           src="https://wallpapermemory.com/uploads/489/wizard-background-hd-1080p-74705.jpg"
           alt=""
         />
-        <div className="join-random-btl">
-          <div className="create-battle">
-            <Link to={BATTLE}>
-              <button
-                className="create-battle-btn"
-                onClick={() => {
-                  if (this.props.user.username) {
-                    this.props.createBattle();
-                  }
-                }}
-              >
-                Create New Battle
-              </button>
-            </Link>
-          </div>
-          <Link to={BATTLE}>
-            <button
-              onClick={() => {
-                if (this.props.user.username) {
-                  this.props.joinRandomBattle();
-                }
-              }}
-              className="join-random-btl-btn"
-            >
-              Join A Random Battle
-            </button>
-          </Link>
+        {this.props.user.username ? (
+          <div>
+            <div className="join-random-btl">
+              <div className="create-battle">
+                <Link to={BATTLE}>
+                  <button
+                    className="create-battle-btn"
+                    onClick={() => {
+                      if (this.props.user.username) {
+                        this.props.createBattle();
+                      }
+                    }}
+                  >
+                    Create New Battle
+                  </button>
+                </Link>
+              </div>
+              <Link to={BATTLE}>
+                <button
+                  onClick={() => {
+                    if (this.props.user.username) {
+                      this.props.joinRandomBattle();
+                    }
+                  }}
+                  className="join-random-btl-btn"
+                >
+                  Join A Random Battle
+                </button>
+              </Link>
 
-          <button className="join-random-btl-btn" disabled>
-            Open Battles
-          </button>
-        </div>
-        <div>
-          {this.props.user.username ? (
+              <button className="join-random-btl-btn" disabled>
+                Open Battles
+              </button>
+            </div>
             <div className="join-btl">
               <OpenBattles
                 user={this.props.user}
@@ -57,12 +57,12 @@ class LandingPage extends React.Component {
                 joinOpenBattle={this.props.joinOpenBattle}
               />
             </div>
-          ) : (
-            <div className="sign-in-box">
-              <h1 className="please-sign-in">Please Sign In</h1>
-            </div>
-          )}
-        </div>
+          </div>
+        ) : (
+          <div className="sign-in-box">
+            <h1 className="wizards-of-code">Wizards Of Code</h1>
+          </div>
+        )}
       </div>
     );
   }
