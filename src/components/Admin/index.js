d93d449c5dee28a2e8cbe82e31ed064aadfddcdb
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2019-10-14 10:49:26 -0400

    id

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -54,7 +54,7 @@ const UserList = ({ users }) => (
       {users.map(user => (
         <li key={user.uid}>
           <span>
-            <strong>ID:</strong> {user.uid}
+            <strong>ID</strong> {user.uid}
           </span>
           <span>
             <strong>E-Mail:</strong> {user.email}
